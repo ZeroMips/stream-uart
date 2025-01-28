ef7a658fff80254ec4f2217ab47105b4ec2c5431
Dirk Eibach <dirk.eibach@gmx.de>
2025-01-28 21:30:28 +0100

    Add loopback to __init__.py

--- src/amaranth_stream_uart/__init__.py
+++ src/amaranth_stream_uart/__init__.py
@@ -2,16 +2,24 @@ from amaranth import *
 
 from amaranth_boards.icestick import ICEStickPlatform
 from amaranth_boards.versa_ecp5 import VersaECP5Platform
+from amaranth.lib import stream,wiring
+from amaranth.lib.wiring import In, Out
 
 from .tx import UARTTransmitter
+from .rx import UARTReceiver
 
 
 class Toplevel(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
-        m.submodules.uart = uart = UARTTransmitter(divider=int(platform.default_clk_frequency//115200))
-        m.d.comb += platform.request("uart", 0).tx.o.eq(uart.tx)
+        m.submodules.uart_tx = uart_tx = UARTTransmitter(divider=217)
+        m.submodules.uart_rx = uart_rx = UARTReceiver(divider=217)
+        uart_resource = platform.request("uart", 0)
+        m.d.comb += uart_resource.tx.o.eq(uart_tx.tx)
+        m.d.comb += uart_rx.rx.eq(uart_resource.rx.i)
+
+        wiring.connect(m, uart_rx=uart_rx.stream, uart_tx=uart_tx.stream)
 
         return m
 
